162fa840f35f2743d960e8663fe291f072caa5a2
rafiakbar13 <muhammadrafia13@gmail.com>
2023-12-31 22:28:39 +0700

    remove error page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,9 +2,7 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
 
-type Props = {};
-
-const NotFoundPage = (props: Props) => {
+const NotFoundPage = () => {
   return (
     <div className="h-full flex flex-col space-y-4 items-center justify-center text-muted-foreground">
       <h1 className="text-4xl">404</h1>
